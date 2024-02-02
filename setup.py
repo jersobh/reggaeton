8d2f172ca5680c3c2632649aa232e68315d18efe
jesantos <jesantos@newstore.com>
2024-02-02 13:11:04 +0000

    feat: bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="reggaeton",
-    version="0.1.0",
+    version="0.1.2",
     author="jersobh",
     author_email="jersobh@gmail.com",
     description="Reggaeton is simple image comparison tool for detecting regressions in images.",
